c496d3f503e9c19251a25eff20683caeb7eb192b
cdiot <christopher.diot5@gmail.com>
2021-07-14 10:30:35 +0200

    Add render Twig #8

---
+++ src/Controller/Controller.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Twig\Loader\FilesystemLoader;
+use Twig\Environment;
+
+/**
+ * Controller 
+ */
+class Controller
+{
+    public function view(string $path, array $datas = [])
+    {
+        $loader = new FilesystemLoader('/templates');
+        $twig = new Environment($loader, [
+            'cache' => false,
+        ]);
+
+        echo $twig->render($path.'.html.twig', $datas);
+    }
+}
